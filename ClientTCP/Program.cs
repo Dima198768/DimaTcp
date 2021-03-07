e988735b9d01e74445733c0d556092b91fea22c8
Makarukha_D <Makarukha_D@HOME-PC>
2021-03-07 14:26:33 +0300

    tcp

--- ClientTCP/Program.cs
+++ ClientTCP/Program.cs
@@ -7,14 +7,14 @@ namespace ClientTCP
     class Program
     {
         static ServerClass server; // сервер
-        static Thread listenThread; // потока для прослушивания
+        static Thread listenThread; // gjnsr lkz ghjcke[jdedfyyz
         static void Main(string[] args)
         {
             try
             {
                 server = new ServerClass();
                 listenThread = new Thread(new ThreadStart(server.Listen));
-                listenThread.Start(); //старт потока
+                listenThread.Start(); //старт потокe
             }
             catch (Exception ex)
             {
